c8adfb3003f6afb5409bf234f6c5b44db2c37f8a
JungJungIn <call518@gmail.com>
2025-06-23 21:32:23 +0900

    Update run scripts

--- run-docker-vllm---Qwen2.5-7B-Instruct.sh
+++ run-docker-vllm---Qwen2.5-7B-Instruct.sh
@@ -2,7 +2,7 @@
 
 #HF_TOKEN=$(cat ~/.huggingface/token)
 
-docker run -d \
+docker_args=(
     --name vLLM-Qwen2.5-7B-Instruct \
     --runtime nvidia \
     --gpus all \
@@ -10,15 +10,19 @@ docker run -d \
     --ipc=host \
     -v ./models:/vllm-workspace/models \
     -v ./config:/vllm-workspace/config \
-    -e HUGGING_FACE_HUB_TOKEN=$HF_TOKEN \
+    #-e HUGGING_FACE_HUB_TOKEN=$HF_TOKEN \
     vllm/vllm-openai:latest \
     --model models/Qwen2.5-7B-Instruct/Qwen2.5-7B-Instruct-Q4_K_M.gguf \
     --tokenizer Qwen/Qwen2.5-7B-Instruct \
     --host "0.0.0.0" \
     --port 5000 \
     --gpu-memory-utilization 1.0 \
+    #--cpu-offload-gb 16 \
     --served-model-name "vLLM-Qwen2.5-7B-Instruct" \
     --max-num-batched-tokens 8192 \
     --max-num-seqs 256 \
     --max-model-len 8192 \
     --generation-config config/Qwen2.5-7B-Instruct
+)
+
+docker run -d "${docker_args[@]}"
